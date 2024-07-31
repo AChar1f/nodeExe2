e88ae47f2645d563d6419f51c86a1c2c44264eec
AChar1f <acharif41@gmail.com>
2024-07-31 13:20:00 +0200

    node exercise 2

---
+++ index.js
@@ -0,0 +1,7 @@
+import { Person }from './Person.js'
+
+const Person1 =  new Person('Abdurahmaan', 'Charif', 'acharif41@gmail.com');
+const Person2 =  new Person('Mustafa', 'Moerat', 'mm@gmail.com');
+const Person3 =  new Person('Qaasiem', 'Harliem', 'qaasiemh@gmail.com');
+
+console.log(Person1, Person2, Person3)
